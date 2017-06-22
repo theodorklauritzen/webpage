cafd4a37744942c986c74d4af604b0005b8dd8bd
xupertheo <xupertheo@hotmail.com>
2017-06-22 15:53:26 +0200

    Last commit before the first release

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-  res.send("express works!");
+router.get("/secret", function(req, res, next) {
+  res.send("This is secret, sort of");
 });
 
 module.exports = router;
