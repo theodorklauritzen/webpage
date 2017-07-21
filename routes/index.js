e1030d68d46b7ecffdcdec44660be331e760edeb
Theodor K. Lauritzen <xupertheo@hotmail.com>
2017-07-21 22:41:46 +0200

    Added files for authentiation, and wrote the html files.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 var router = express.Router();
 
+router.use("/user", require("./user.js"))
+
 router.get("/secret", function(req, res, next) {
   res.send("This is secret, sort of");
 });
